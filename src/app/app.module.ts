3cc9cb7a4f8df8d124ad37d00173ac0b0163d0e0
gbloch <gaetan.bloch@gmail.com>
2021-06-26 19:36:11 +0200

    init(): material design, form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CollectionFormComponent } from './collection-form/collection-form.component';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CollectionFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/collection-form/collection-form.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from "@angular/forms";
+
+@Component({
+  selector: 'app-collection-form',
+  templateUrl: './collection-form.component.html',
+  styleUrls: ['./collection-form.component.scss']
+})
+export class CollectionFormComponent implements OnInit {
+  form = new FormGroup({
+    collection: new FormControl('')
+  });
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    console.log(this.form.value)
+  }
+}
